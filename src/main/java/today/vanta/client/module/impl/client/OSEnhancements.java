a1aea6fb2f18d72f8156fbcae6eb4f96b8047fc9
6x68 <249649284+fun-fact-is-that-everything-after-the-plus-sign-is-useless@users.noreply.github.com>
2026-07-08 16:13:24 -0500

    refactor(OSEnhancements/module): stuff

--- src/main/java/today/vanta/client/module/impl/client/OSEnhancements.java
+++ src/main/java/today/vanta/client/module/impl/client/OSEnhancements.java
@@ -1,11 +1,13 @@
 package today.vanta.client.module.impl.client;
 
 import net.minecraft.util.Util;
+import today.vanta.client.event.impl.game.render.DisplayGuiScreenEvent;
 import today.vanta.client.module.Category;
 import today.vanta.client.module.Module;
 import today.vanta.client.setting.Setting;
 import today.vanta.client.setting.impl.BooleanSetting;
 import today.vanta.client.setting.impl.StringSetting;
+import today.vanta.util.game.events.EventListen;
 import today.vanta.util.os.windows.WindowsOS;
 import today.vanta.util.os.windows.enhancements.impl.*;
 
@@ -14,6 +16,7 @@ public class OSEnhancements extends Module {
     public final StringSetting backdrop = Setting.of("Backdrop", "Auto", "Auto", "None", "Mica", "Acrylic", "Tabbed");
     public final BooleanSetting transparent = Setting.of("Transparent", true);
     public final StringSetting windowCorner = Setting.of("Window corner", "Default", "Default", "Don't round", "Round", "Round (Small)");
+    public final BooleanSetting colorMask = Setting.of("Color mask", false);
 
     public OSEnhancements() {
         super("OSEnhancements", "Configure OS-specific window enhancements.", Category.CLIENT);
@@ -24,14 +27,45 @@ public class OSEnhancements extends Module {
             darkTitleBar.addListener((setting, oldValue, newValue) ->
                     DarkTitleBar.INSTANCE.apply(WindowsOS.INSTANCE, newValue));
 
-            backdrop.addListener((setting, oldValue, newValue) ->
-                    SystemBackdrop.INSTANCE.apply(WindowsOS.INSTANCE, parseBackdrop(newValue)));
+            backdrop.addListener((setting, oldValue, newValue) -> {
+                if (!colorMask.getValue() || mc.currentScreen != null) {
+                    SystemBackdrop.INSTANCE.apply(WindowsOS.INSTANCE, parseBackdrop(newValue));
+                }
+            });
 
-            transparent.addListener((setting, oldValue, newValue) ->
-                    Transparent.INSTANCE.apply(WindowsOS.INSTANCE, newValue));
+            transparent.addListener((setting, oldValue, newValue) -> {
+                if (newValue) {
+                    Transparent.INSTANCE.apply(WindowsOS.INSTANCE, mc.currentScreen != null);
+                } else {
+                    Transparent.INSTANCE.apply(WindowsOS.INSTANCE, false);
+                }
+            });
 
             windowCorner.addListener((setting, oldValue, newValue) ->
                     WindowCorner.INSTANCE.apply(WindowsOS.INSTANCE, parseCorner(newValue)));
+
+            colorMask.addListener((setting, oldValue, newValue) -> {
+                if (mc.currentScreen == null) {
+                    SystemBackdrop.INSTANCE.apply(WindowsOS.INSTANCE, newValue ? SystemBackdrop.Type.NONE : parseBackdrop(backdrop.getValue()));
+                }
+            });
+
+            if (transparent.getValue() && mc.currentScreen != null) {
+                Transparent.INSTANCE.apply(WindowsOS.INSTANCE, true);
+            }
+        }
+    }
+
+    @EventListen
+    private void onDisplayGuiScreen(DisplayGuiScreenEvent event) {
+        boolean inMenu = event.screen != null;
+
+        if (transparent.getValue()) {
+            Transparent.INSTANCE.apply(WindowsOS.INSTANCE, inMenu);
+        }
+
+        if (colorMask.getValue()) {
+            SystemBackdrop.INSTANCE.apply(WindowsOS.INSTANCE, inMenu ? parseBackdrop(backdrop.getValue()) : SystemBackdrop.Type.NONE);
         }
     }
 
