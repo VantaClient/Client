0e25c07567843134e27d66f033b9f52b12ec1e6b
desync_lord <mark@wonderland.ac>
2026-07-21 21:41:01 +0300

    chore: update changelog

--- src/main/java/today/vanta/util/client/Strings.java
+++ src/main/java/today/vanta/util/client/Strings.java
@@ -10,13 +10,16 @@ public interface Strings {
     String CLIENT_FULL_TITLE = CLIENT_NAME + " - " + CLIENT_VERSION + " - " + DEVELOPERS;
 
     List<String> CHANGELOG = Arrays.asList(
+            "[#] Fixed macOS ImGui issues (now optional with env var ``DISABLE_IMGUI=true``)",
             "[#] Fixed Boxy ClickGUI size & position not saving",
             "[#] Fixed glass check checking for barriers",
-            "[#] Fixed macOS ImGui font atlas issue",
+            "[#] Fixed potential ViaVersion issues",
             "[#] Fixed Boxy ClickGUI stutterness",
             "[#] Fixed module event crashes",
             "[+] Added StaffDetector",
+            "[~] Added display names for ESP & ChestESP",
             "[~] Moved NoWeb to separate module class",
-            "[~] Moved ImGui ini file path"
+            "[~] Moved ImGui ini file path",
+            "[~] Changed VClip wording"
     );
 }
