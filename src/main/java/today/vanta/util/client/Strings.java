a7e77c282efb85b3b3c46adbcb51d2b6c5a95947
desync_lord <mark@wonderland.ac>
2026-07-21 21:48:08 +0300

    chore: bump version

--- src/main/java/today/vanta/util/client/Strings.java
+++ src/main/java/today/vanta/util/client/Strings.java
@@ -5,21 +5,11 @@ import java.util.List;
 
 public interface Strings {
     String CLIENT_NAME = "Vanta";
-    String CLIENT_VERSION = "1.7";
+    String CLIENT_VERSION = "1.8";
     String DEVELOPERS = "made by mark & luna";
     String CLIENT_FULL_TITLE = CLIENT_NAME + " - " + CLIENT_VERSION + " - " + DEVELOPERS;
 
     List<String> CHANGELOG = Arrays.asList(
-            "[#] Fixed macOS ImGui issues (now optional with env var ``DISABLE_IMGUI=true``)",
-            "[#] Fixed Boxy ClickGUI size & position not saving",
-            "[#] Fixed glass check checking for barriers",
-            "[#] Fixed potential ViaVersion issues",
-            "[#] Fixed Boxy ClickGUI stutterness",
-            "[#] Fixed module event crashes",
-            "[+] Added StaffDetector",
-            "[~] Added display names for ESP & ChestESP",
-            "[~] Moved NoWeb to separate module class",
-            "[~] Moved ImGui ini file path",
-            "[~] Changed VClip wording"
+
     );
 }
